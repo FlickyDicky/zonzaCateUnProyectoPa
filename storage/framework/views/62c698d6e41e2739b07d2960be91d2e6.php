e0df8593efb06d9f4a2293a59138b7c37aa5be1c
Ryan Buck <rbuckbamford@gmail.com>
2023-11-27 16:02:21 +0000

    Latest version with / route correction for both admin and non-admin, also corrected the welcome blade view

--- storage/framework/views/62c698d6e41e2739b07d2960be91d2e6.php
+++ storage/framework/views/62c698d6e41e2739b07d2960be91d2e6.php
@@ -836,16 +836,13 @@
         <?php if(Route::has('login')): ?>
             <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                 <?php if(auth()->guard()->check()): ?>
-                    <?php if(auth()->user()->is_admin): ?>
-                        <a href="<?php echo e(url('/admin/dashboard')); ?>"
-                            class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Admin Dashboard</a>
-                    <?php else: ?>
-                        <a href="<?php echo e(url('/user/dashboard')); ?>"
-                            class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">User Dashboard</a>
-                    <?php endif; ?>
+                    <a href="<?php echo e(url('/dashboard')); ?>"
+                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">User
+                        Dashboard</a>
                 <?php else: ?>
                     <a href="<?php echo e(route('login')); ?>"
-                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
+                        in</a>
 
                     <?php if(Route::has('register')): ?>
                         <a href="<?php echo e(route('register')); ?>"
